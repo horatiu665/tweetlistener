94d1a662f7c1e36b591cb5e5cc544cb30560e893
Horatiu at KU <horatiu665@yahoo.com>
2015-07-02 18:34:05 +0200

    added the .bat used to initialize the search for all the 12 games.

---
+++ WPFTwitter/WPFTwitter/bin/demo run multiple tweetlisteners ALL.bat
@@ -0,0 +1,205 @@
+ start "WPFTwitter" /D "Release/" "Release/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "riseofincarnates" ^
+ /saveToDatabase 0 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 23:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 0 ^
+ /keywords "#riseofincarnates riseincarnates" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (1)/" "Release - Copy (1)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "rorymcilroy" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 1 ^
+ /keywords "#pgatour #nextgengolf #rorymcilroy #rorymcilroypgatour" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (2)/" "Release - Copy (2)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "godzilla" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 2 ^
+ /keywords "#godzilla #godzillavideogame #godzillathegame #kaijuroar" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (3)/" "Release - Copy (3)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "formula1" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 3 ^
+ /keywords "#formula1game #f12015 formula1game" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (4)/" "Release - Copy (4)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "paddington" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 4 ^
+ /keywords "#paddington #paddingtongame #paddingtonadventuresinlondon" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (5)/" "Release - Copy (5)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "madden16" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 5 ^
+ /keywords "#madden16" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (6)/" "Release - Copy (6)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "armikrog" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 6 ^
+ /keywords "#armikrog armikrog" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (7)/" "Release - Copy (7)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "swindle" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 7 ^
+ /keywords "#swindle #theswindle theswindle" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (8)/" "Release - Copy (8)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "kingsquest" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 8 ^
+ /keywords "#kingsquest" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (9)/" "Release - Copy (9)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "eisenwald" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 9 ^
+ /keywords "#legendsofeisenwald legendsofeisenwald" ^
+ /startStream 1
+
+ start "WPFTwitter" /D "Release - Copy (10)/" "Release - Copy (10)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "zombievikings" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 10 ^
+ /keywords "#zombievikings zombievikings" ^
+ /startStream 1
+
+  start "WPFTwitter" /D "Release - Copy (11)/" "Release - Copy (11)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "everybodyrapture" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 11 ^
+ /keywords "#everybodysgonetotherapture" ^
+ /startStream 1
+
+ 
