79684d45356d6d77e89139c406ca09849b6c1a02
horatiu665 <horatiu665@yahoo.com>
2015-09-30 14:47:56 +0200

    The batch file is being worked on.... first 10 games added! More to come before we deploy.

--- WPFTwitter/WPFTwitter/bin/Release/demo.bat
+++ WPFTwitter/WPFTwitter/bin/Release/demo.bat
@@ -1,4 +1,4 @@
- start "WPFTwitter" "Release/WPFTwitter.exe" ^
+ start "WPFTwitter" "WPFTwitter.exe" ^
  /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
  /logPath "log.txt" ^
  /textFileDbPath "rawJsonBackup.txt" ^
@@ -20,29 +20,7 @@
  /windowTitle "Game test 1" ^
  /startStream 0
 
-  start "WPFTwitter" "Release - Copy/WPFTwitter.exe" ^
- /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
- /logPath "log.txt" ^
- /textFileDbPath "rawJsonBackup.txt" ^
- /dbTableName "gametest2" ^
- /dbDestination "php" ^
- /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
- /saveToDatabase 0 ^
- /saveToTextFile 1 ^
- /saveToRam 1 ^
- /outputEventCounters 1 ^
- /outputDatabaseMessages 0 ^
- /logEveryJson 0 ^
- /emailDisco 06:00:00 ^
- /emailConnected 23:59:59 ^
- /onlyEnglish 1 ^
- /onlyWithHashtags 0 ^
- /credentials 3 ^
- /keywords "mysql, gingernigger, google spartan" ^
- /windowTitle "Game test 2222222222222222222222222222222222222222" ^
- /startStream 0
-
-  start "WPFTwitter" "Release - Copy (2)/WPFTwitter.exe" ^
+  start "WPFTwitter2" "WPFTwitter2.exe" ^
  /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
  /logPath "log.txt" ^
  /textFileDbPath "rawJsonBackup.txt" ^
---
+++ WPFTwitter/WPFTwitter/bin/run oct-dec games.bat
@@ -0,0 +1,221 @@
+ start "WPFTwitter" "Release/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "1yoshi" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 1 ^
+ /keywords "#YoshiWoollyWorld, #YoshisWoollyWorld, #yoshiwoolyworld,#yoshiswoolyworld, #woollyworld, #woolyworld" ^
+ /windowTitle "Yoshi's Woolly World" ^
+ /startStream 1
+
+ start "WPFTwitter" "Release - Copy/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "2dbz" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 2 ^
+ /keywords "#dbzeb, #extremebutoden, #DragonBallZExtremeButoden" ^
+ /windowTitle "DragonBallZ" ^
+ /startStream 1
+
+ start "WPFTwitter" "Release - Copy (2)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "3guitarhero" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 3 ^
+ /keywords "#GuitarHeroLive, guitar hero live" ^
+ /windowTitle "Guitar Hero Live" ^
+ /startStream 1
+ 
+ start "WPFTwitter" "Release - Copy (3)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "4lifeisstrange" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 4 ^
+ /keywords "life is strange polarized, #LifeisStrange #Polarized, #LISFinale " ^
+ /windowTitle "life is strange" ^
+ /startStream 1
+ 
+ start "WPFTwitter" "Release - Copy (4)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "5rebelgalaxy" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 5 ^
+ /keywords "rebelgalaxy" ^
+ /windowTitle "rebel galaxy" ^
+ /startStream 1
+
+ start "WPFTwitter" "Release - Copy (5)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "6talesofzestiria" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 6 ^
+ /keywords "#TalesofZestiria, Tales of Zestiria, #zestiria" ^
+ /windowTitle "Tales of Zestiria" ^
+ /startStream 1
+
+ start "WPFTwitter" "Release - Copy (6)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "7swordcoastlegends" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 7 ^
+ /keywords "#swordcoastlegends, #swordcoast, @swordcoast" ^
+ /windowTitle "sword coast legends" ^
+ /startStream 1
+
+ start "WPFTwitter" "Release - Copy (7)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "8fatalframe" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 8 ^
+ /keywords "Fatal Frame Maiden of Black Water, #FatalFrame5, #maidenofblackwater" ^
+ /windowTitle "Fatal Frame Maiden of Black Water" ^
+ /startStream 1
+
+ start "WPFTwitter" "Release - Copy (8)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "9assassinscreed" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 9 ^
+ /keywords "#AssassinsCreedSyndicate, #syndicate" ^
+ /windowTitle "Assassins Creed Syndicate" ^
+ /startStream 1
+
+ start "WPFTwitter" "Release - Copy (9)/WPFTwitter.exe" ^
+ /phpPostPath "http://localhost/hhh/tweetlistenerweb/php/saveJson.php" ^
+ /logPath "log.txt" ^
+ /textFileDbPath "rawJsonBackup.txt" ^
+ /dbTableName "10legendofzelda" ^
+ /dbDestination "mysql" ^
+ /dbConnectionStr "server=localhost;userid=root;password=;database=twitter" ^
+ /saveToDatabase 1 ^
+ /saveToTextFile 1 ^
+ /saveToRam 0 ^
+ /outputEventCounters 0 ^
+ /outputDatabaseMessages 0 ^
+ /logEveryJson 0 ^
+ /emailDisco 06:00:00 ^
+ /emailConnected 23:59:59 ^
+ /onlyEnglish 1 ^
+ /onlyWithHashtags 0 ^
+ /credentials 10 ^
+ /keywords "#triforceheroes, tri force heroes" ^
+ /windowTitle "Legend of Zelda: Tri Force Heroes" ^
+ /startStream 1
+ 
+ 
